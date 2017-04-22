fbf0930dd8ca170006d4ab96d456ad3b70e6d641
xNerhu22 <xnerhu22@onet.pl>
2017-04-22 18:07:29 +0200

    Added animation to loading posts

--- 3/src/views/Tabs/Posts/index.js
+++ 3/src/views/Tabs/Posts/index.js
@@ -40,43 +40,6 @@ export default class PostsTab extends React.Component {
   }
 
   componentDidMount () {
-    /*this.setState({
-      posts: [
-        {
-          id: 2,
-          title: 'Test',
-          author: 'Mikołaj Palkiewicz',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in neque turpis. Aenean tincidunt nunc nec ligula cursus iaculis. Pellentesque nisl nulla, malesuada a est a, tempor dapibus eros. Sed facilisis porta auctor.',
-          date: '14.04.2017 20:38',
-          avatar: 'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/14581320_549947718524540_5437545186607783553_n.jpg?oh=1d709d8978f80d6887041c3e9583f27f&oe=59994281',
-          likes: [
-            {
-              userName: 'Mikołaj Palkiewicz',
-              userID: 1
-            }
-          ],
-          comments: [
-            {
-              author: 'Mikołaj Palkiewicz',
-              userID: 1,
-              content: 'Lorem ipsum dolor sit amet',
-              date: '14.04.2017 21:12',
-              avatar: 'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/14581320_549947718524540_5437545186607783553_n.jpg?oh=1d709d8978f80d6887041c3e9583f27f&oe=59994281'
-            }
-          ]
-        },
-        {
-          id: 1,
-          title: 'Test 2',
-          author: 'Mikołaj Palkiewicz',
-          content: 'Wart.',
-          date: '14.04.2017 10:38',
-          avatar: 'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/14581320_549947718524540_5437545186607783553_n.jpg?oh=1d709d8978f80d6887041c3e9583f27f&oe=59994281',
-          likes: [],
-          comments: []
-        }
-      ]
-    })*/
     this.loadPosts()
   }
 
