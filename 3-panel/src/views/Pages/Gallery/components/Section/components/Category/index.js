63ae22f5528932a048513439ab498182352204f9
xNerhu22 <xnerhu22@onet.pl>
2017-07-06 18:03:37 +0200

    Add ripple, menu icon to category

--- 3-panel/src/views/Pages/Gallery/components/Section/components/Category/index.js
+++ 3-panel/src/views/Pages/Gallery/components/Section/components/Category/index.js
@@ -1,6 +1,10 @@
 import Component from '../../../../../../../helpers/Component'
 
 export default class Category extends Component {
+  beforeRender () {
+    this.touched = false
+  }
+
   /**
    * Gets root.
    * @return {DOMElement} root
@@ -9,6 +13,9 @@ export default class Category extends Component {
     return this.elements.root
   }
 
+  /**
+   * Loads image.
+   */
   loadImage () {
     const root = this.getRoot()
 
@@ -25,20 +32,97 @@ export default class Category extends Component {
     img.src = this.props.data.pictures[0]
   }
 
+  /**
+   * On mouse down event.
+   * Makes ripple.
+   * @param {Event}
+   */
+  onMouseDown = (e) => {
+    const root = this.getRoot()
+    const target = e.target
+
+    if (target !== this.elements.title && target !== this.elements.menuIcon && !this.touched) {
+      const ripple = Ripple.createRipple(root, this.props.rippleStyle, createRippleMouse(root, e, 1.5))
+      Ripple.makeRipple(ripple)
+    }
+  }
+
+  /**
+   * On touch start event (on mobile).
+   * Makes ripple.
+   * @param {Event}
+   */
+  onTouchStart = (e) => {
+    const root = this.getRoot()
+
+    if (target !== this.elements.title && target !== this.elements.menuIcon) {
+      const ripple = Ripple.createRipple(root, this.props.rippleStyle, createRippleMouse(root, e, 1.5, true))
+      Ripple.makeRipple(ripple)
+    }
+
+    this.touched = true
+  }
+
+  /**
+   * On menu icon mouse down event.
+   * Makes ripple.
+   * @param {Event}
+   */
+  onMenuIconMouseDown = (e) => {
+    const menuIcon = this.elements.menuIcon
+
+    if (!this.touched) {
+      const ripple = Ripple.createRipple(menuIcon, this.props.menuIconRippleStyle, createRippleCenter(menuIcon, 14))
+      Ripple.makeRipple(ripple)
+    }
+  }
+
+  /**
+   * On menu icon touch start event (on mobile).
+   * Makes ripple.
+   * @param {Event}
+   */
+  onMenuIconTouchStart = (e) => {
+    const menuIcon = this.elements.menuIcon
+
+    const ripple = Ripple.createRipple(menuIcon, this.props.menuIconRippleStyle, createRippleCenter(menuIcon, 14, 0.4, true))
+    Ripple.makeRipple(ripple)
+
+    this.touched = true
+  }
+
   render () {
     return (
-      <div className='page-gallery-category' ref='root'>
-        <div className='title'>
+      <div className='page-gallery-category ripple' ref='root' onMouseDown={this.onMouseDown} onTouchStart={this.onTouchStart}>
+        <div className='title' ref='title'>
           {
             this.props.data.name
           }
-          <div className='menu-icon' />
+          <div className='menu-icon ripple-icon' ref='menuIcon' onMouseDown={this.onMenuIconMouseDown} onTouchStart={this.onMenuIconTouchStart} />
         </div>
       </div>
     )
   }
 
   afterRender () {
+    const props = this.props
+
+    if (props.ripple == null) props.ripple = true
+
+    if (props.rippleStyle == null) {
+      props.rippleStyle = {
+        backgroundColor: '#fff',
+        opacity: 0.2
+      }
+    }
+
+    if (props.menuIconRippleStyle == null) {
+      props.menuIconRippleStyle = {
+        backgroundColor: '#000',
+        opacity: 0.2
+      }
+    }
+
     this.loadImage()
   }
 }
